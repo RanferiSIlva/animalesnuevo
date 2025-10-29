30bd67cfc3dc10ec7c91c4a8b0e01e176dfc28c8
Prashanti355 <shantipa95@gmail.com>
2025-10-29 09:21:27 -0600

    Add files via upload

---
+++ animales.py
@@ -0,0 +1,21 @@
+# animales.py
+# Ejemplo simple para practicar pull requests en GitHub
+# Cada colaborador puede agregar un nuevo animal a la lista
+
+def mostrar_animales():
+    animales = [
+        "Perro",
+        "Gato",
+        "Loro",
+	"Vaca",
+	"Mosquito",
+        "serpiente",
+	"Toro",
+	"Lobo"
+    ]
+    for a in animales:
+        print("Animal:", a)
+
+if __name__ == "__main__":
+    print("Bienvenido al zoológico colaborativo 🐾")
+    mostrar_animales()
